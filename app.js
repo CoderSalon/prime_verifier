f7b682642395037f63c65604d5d85da8c9bb9885
Kevin <kevin78515@gmail.com>
2015-05-06 18:05:23 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+verifyButton.onclick = function(){
+	var number = inputNumber.value, // number為輸入框的文字
+		isPrime = true;
+
+	// ==== 以下是你要寫程式的地方 ==== //
+
+
+
+
+	// ====    以上是你的程式    ==== //
+
+	// 	執行到這裡以後...
+	// 	當number是質數時，isPrime應該是 true
+	//  當number不是質數時，isPrime應該是 false
+
+	if(isPrime){
+		result.innerHTML=number+"是一個質數！";
+	} else {
+		result.innerHTML=number+"不是一個質數！";
+	}
+}
